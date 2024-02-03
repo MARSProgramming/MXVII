1863236e03ea9e57e3bfb7f5e93795b028bbf193
Shawn <shmorganl14@gmail.com>
2024-02-02 19:47:13 -0500

    not all changes were committed

--- robot code/src/main/java/frc/robot/commands/AmpSetpoint.java
+++ robot code/src/main/java/frc/robot/commands/AmpSetpoint.java
@@ -1,6 +1,6 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.DynamicConstants;
 import frc.robot.subsystems.IntakePivot;
 import frc.robot.subsystems.IntakeWheels;
