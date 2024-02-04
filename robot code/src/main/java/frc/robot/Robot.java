ebe77359923e085781d93f6a145eaa1b5e3b9962
Shawn <shmorganl14@gmail.com>
2024-02-04 13:39:11 -0500

    added super basic logging

--- robot code/src/main/java/frc/robot/Robot.java
+++ robot code/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -33,6 +35,8 @@ public class Robot extends TimedRobot {
         DynamicConstants.init();
         DynamicConstants.periodic();
         m_robotContainer = new RobotContainer();
+        DataLogManager.start();
+        DriverStation.startDataLog(DataLogManager.getLog());
     }
 
     /**
